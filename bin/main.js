aa3630203b8887ccc4234ea1f770f3851cf915b7
EricJurbergs <erj0008@uah.edu>
2024-10-14 00:50:41 -0500

    Added a basic function to find the first document with an attribute and value key, value pair passed by the caller. This can likely be used to find an object from a SKU

--- bin/main.js
+++ bin/main.js
@@ -1,1 +1,21 @@
-mongo = require('mongodb');
+const {MongoClient} = require('mongodb');
+
+async function main() {
+    //not sure the best way to handle securely getting the uri for the mongo deployment
+}
+
+main().catch(console.error);
+
+
+async function findOneListingByAttr(client, nameOfDatabase, nameOfCollection, attr, attrValue){
+    let query = {};
+    query[attr] = attrValue;
+    const result=await client.db(nameOfDatabase).collection(nameOfCollection).findOne(query)
+
+    if (result){
+        console.log(`Found result with ${attr} ${attrValue}`)
+        console.log(result)
+    } else{
+        console.log("No Result")
+    }
+}
